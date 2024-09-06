b0421c81090a6fdeeebeb5e3a4e3c9a3edcc4863
mspruc <madssejer@gmail.com>
2024-09-06 15:12:20 +0200

    better cli tooling for imdb/jo benchmark

--- wayang-plugins/wayang-ml/src/main/java/org/apache/wayang/ml/benchmarks/IMDBJOBenchmark.java
+++ wayang-plugins/wayang-ml/src/main/java/org/apache/wayang/ml/benchmarks/IMDBJOBenchmark.java
@@ -1,18 +1,29 @@
 package org.apache.wayang.ml.benchmarks;
 
 import com.google.common.io.Resources;
+
+import org.apache.commons.lang.StringUtils;
 import org.apache.wayang.api.sql.context.SqlContext;
 import org.apache.wayang.basic.data.Record;
 import org.apache.wayang.core.api.Configuration;
 
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Collection;
 
 
 public class IMDBJOBenchmark {
+    /**
+     * Benchmarking tool for the imdb/jo benchmark, Calcite dictates that every jo query follows
+     * the schema, "schema_name.table_name". The tool searches for the queries in resources/calcite-ready-job-queries
+     * @param args args[0]: path to calcite-job-ready-queries/*.sql
+     */
     public static void main(String[] args){
         try {
             Configuration configuration = new Configuration();
+
             configuration.setProperty("wayang.postgres.jdbc.url", "jdbc:postgresql://localhost:5432/job");
             configuration.setProperty("wayang.postgres.jdbc.user", "postgres");
             configuration.setProperty("wayang.postgres.jdbc.password", "postgres");
@@ -24,8 +35,12 @@ public class IMDBJOBenchmark {
 
             SqlContext sqlContext = new SqlContext(configuration);
 
+            Path pathToQuery = Paths.get(args[0]);
+            String query = StringUtils.chop(Files.readString(pathToQuery).stripTrailing()); //need to chop off the last ';' otherwise sqlContext cant parse it
+            System.out.println("Read query: " + query);
+            
             Collection<Record> result = sqlContext.executeSql(
-                "select * from job.aka_name"
+                query
             );
 
             result.stream().forEach(e -> System.out.println(e));
@@ -35,3 +50,4 @@ public class IMDBJOBenchmark {
     }
     
 }
+ 
