60698a1830cde1cf74f81be4350ffc62b736b2fb
mspruc <madssejer@gmail.com>
2024-09-11 16:03:18 +0200

    added missing configuration property

--- wayang-plugins/wayang-ml/src/main/java/org/apache/wayang/ml/benchmarks/IMDBJOBenchmark.java
+++ wayang-plugins/wayang-ml/src/main/java/org/apache/wayang/ml/benchmarks/IMDBJOBenchmark.java
@@ -43,6 +43,7 @@ public class IMDBJOBenchmark {
                 "mlo" + ".txt"
             );
 
+            configuration.setProperty("wayang.ml.experience.enabled", "false");
 
 
             SqlContext sqlContext = new SqlContext(configuration);
