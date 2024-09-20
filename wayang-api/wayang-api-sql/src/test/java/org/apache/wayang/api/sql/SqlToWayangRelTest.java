c03a6bf5ba382e75633348087ee2dd96973176d9
mspruc <madssejer@gmail.com>
2024-09-20 15:55:10 +0200

    Test for queries with NOT operator

--- wayang-api/wayang-api-sql/src/test/java/org/apache/wayang/api/sql/SqlToWayangRelTest.java
+++ wayang-api/wayang-api-sql/src/test/java/org/apache/wayang/api/sql/SqlToWayangRelTest.java
@@ -42,11 +42,45 @@ import org.junit.Test;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 
 public class SqlToWayangRelTest {
-
     @Test
+    public void filterWithNotLike() throws Exception {
+        String calciteModelPath = SqlAPI.class.getResource("/model-example-min.json").getPath();
+        
+        System.out.println("loading calcite model: " + calciteModelPath);
+        Configuration configuration = new ModelParser(new Configuration(), calciteModelPath).setProperties();
+
+        String dataPath = SqlAPI.class.getResource("/data/largeLeftTableIndex.csv").getPath();
+        configuration.setProperty("wayang.fs.table.url", dataPath);
+
+        configuration.setProperty(
+                "wayang.ml.executions.file",
+                "mle" + ".txt"
+            );
+
+        configuration.setProperty(
+        "wayang.ml.optimizations.file",
+        "mlo" + ".txt"
+        );
+
+        configuration.setProperty("wayang.ml.experience.enabled", "false");
+
+        SqlContext sqlContext = new SqlContext(configuration);
+
+
+        Collection<org.apache.wayang.basic.data.Record> result = sqlContext.executeSql(
+            "SELECT * FROM fs.largeLeftTableIndex WHERE (largeLeftTableIndex.NAMEA NOT LIKE '_est1')" //
+        );
+
+        System.out.println("Printing results");
+        result.stream().forEach(System.out::println);
+        assert(!result.stream().anyMatch(record -> record.getString(0).equals("test1")));
+    }
+
+    //@Test
     public void filterWithLike() throws Exception {
         String calciteModelPath = SqlAPI.class.getResource("/model-example-min.json").getPath();
         
