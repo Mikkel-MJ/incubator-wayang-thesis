34a0228819d28166d5003a412cb6a0507948aa4e
mspruc <madssejer@gmail.com>
2024-10-15 17:05:10 +0200

    generified join keyExtractor and added javadocs

--- wayang-api/wayang-api-sql/src/main/java/org/apache/wayang/api/sql/calcite/converter/JoinHelpers/KeyExtractor.java
+++ wayang-api/wayang-api-sql/src/main/java/org/apache/wayang/api/sql/calcite/converter/JoinHelpers/KeyExtractor.java
@@ -1,19 +1,25 @@
 package org.apache.wayang.api.sql.calcite.converter.JoinHelpers;
 
+import java.util.function.Function;
+
 import org.apache.wayang.basic.data.Record;
 import org.apache.wayang.core.function.FunctionDescriptor;
 
-/**
- * Extracts the key
- */
-public class KeyExtractor implements FunctionDescriptor.SerializableFunction<Record, Object> {
+public class KeyExtractor<T> implements FunctionDescriptor.SerializableFunction<Record, T> {
     private final int index;
 
+    /**
+     * Extracts a key for a joinOperator.
+     * is a subtype of {@link Function}, {@link Serializable} (as required by engines which use serialisation i.e. flink/spark)
+     * Takes an input {@link Record} & {@link Integer} key and maps it to a generic field object T.
+     * Performs an unchecked cast when applied.
+     * @param index key
+     */
     public KeyExtractor(final int index) {
         this.index = index;
     }
 
-    public Object apply(final Record record) {
-        return record.getField(index);
+    public T apply(final Record record) {
+        return (T) record.getField(index);
     }
 }
