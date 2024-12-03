12bc6b90b00947326b4b2395e1bd37079440840c
Juri Petersen <juri@hey.com>
2024-12-03 12:55:25 +0100

    Add IMDB JOB runner

--- bin/runners/postgres/runner.sh
+++ bin/runners/postgres/runner.sh
@@ -4,16 +4,16 @@ schema_path=/work/lsbo-paper/data/JOBenchmark/schema
 
 echo "Setting up postgres schema"
 
-psql -U ucloud -d job -a -f $schema_path/1_schema.sql
+psql -h postgres -U ucloud -d job -a -f $schema_path/1_schema.sql
 
 echo "Adding FK indexes"
 
-psql -U ucloud -d job -a -f $schema_path/2_fkindexes.sql
+psql -h postgres -U ucloud -d job -a -f $schema_path/2_fkindexes.sql
 
 echo "Copying data"
 
-psql -U ucloud -d job -a -f $schema_path/3_copy_data.sql
+psql -h postgres -U ucloud -d job -a -f $schema_path/3_copy_data.sql
 
 echo "Add FKs"
 
-psql -U ucloud -d job -a -f $schema_path/4_add_fks.sql
+psql -postgres -U ucloud -d job -a -f $schema_path/4_add_fks.sql
