55d8218615c84ea3e65ed0f1792d492cd71bfb70
mspruc <madssejer@gmail.com>
2024-12-16 09:40:56 +0100

    added a way of dealiasing projections & aggregates for calcite's relNodes

--- wayang-api/wayang-api-sql/src/main/java/org/apache/wayang/api/sql/calcite/utils/AliasFinder.java
+++ wayang-api/wayang-api-sql/src/main/java/org/apache/wayang/api/sql/calcite/utils/AliasFinder.java
@@ -4,9 +4,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.type.RelDataTypeField;
+
 import org.apache.wayang.api.sql.calcite.converter.TableScanVisitor;
+import org.apache.wayang.api.sql.calcite.rel.WayangAggregate;
+import org.apache.wayang.api.sql.calcite.rel.WayangProject;
 
 public class AliasFinder {
     public final List<RelDataTypeField> catalog;
@@ -50,5 +55,52 @@ public class AliasFinder {
         this.columnIndexToTableName = columnIndexToTableName;
         this.columnToTableNameMap = columnToTableNameMap;
         this.tableOccurenceCounter = tableOccurenceCounter;
+    }
+
+    /**
+     * Searches through {@link WayangAggregate} and {@link WayangProject} chains.
+     * These nodes uses aliases which have to be unpacked by finding the last
+     * non aliased input. This input still contains Calcite's unique integer
+     * identifier at the end of the column name which will have to be removed.
+     * 
+     * @param startNode     starting {@link WayangAggregate} or
+     *                      {@link WayangProject}
+     * @param columnIndexes starting column indexes that you want to de-alias
+     * @return input of the last {@RelNode} in the chain
+     */
+    public static List<RelDataTypeField> getGlobalCatalogColumnName(final RelNode startNode,
+            final List<Integer> columnIndexes) {
+
+        RelNode currentRelNode = startNode;
+        List<Integer> currentIndexes = columnIndexes;
+
+        while (currentRelNode instanceof WayangAggregate || currentRelNode instanceof WayangProject) {
+            final RelNode next = currentRelNode.getInput(0);
+
+            if (next instanceof WayangAggregate) {
+                final List<Integer> nextIndexes = ((WayangAggregate) next).getAggCallList().stream()
+                        .map(agg -> agg.getArgList().get(0))
+                        .collect(Collectors.toList());
+
+                currentIndexes = currentIndexes.stream()
+                        .map(index -> nextIndexes.get(index))
+                        .collect(Collectors.toList());
+            } else if (next instanceof WayangProject) {
+                final List<Integer> nextIndexes = ((WayangProject) next).getProjects().stream()
+                        .map(proj -> proj.hashCode())
+                        .collect(Collectors.toList());
+
+                currentIndexes = currentIndexes.stream()
+                        .map(index -> nextIndexes.get(index))
+                        .collect(Collectors.toList());
+            }
+            currentRelNode = next;
+        }
+
+        final RelNode selectChainInputNode = currentRelNode;
+
+        return currentIndexes.stream()
+                .map(index -> selectChainInputNode.getRowType().getFieldList().get(index))
+                .collect(Collectors.toList());
     }
 }
