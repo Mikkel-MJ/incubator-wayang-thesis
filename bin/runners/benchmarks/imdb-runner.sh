5dc4dd3d1b870137f4cb64d4292d41c273671f51
Juri Petersen <juri@hey.com>
2025-01-16 14:23:45 +0100

    Fix query splits and add timeout to imdb runner

--- bin/runners/benchmarks/imdb-runner.sh
+++ bin/runners/benchmarks/imdb-runner.sh
@@ -22,7 +22,10 @@ echo "Running JOBenchmark"
 
 #for i in {0..5}; do
     for query in "$test_path"/*.sql; do
-        ./bin/wayang-submit -Xmx32g org.apache.wayang.ml.benchmarks.JOBenchmark java,spark,flink,postgres $data_path/ $timings_path/ $query
+        timeout --kill-after=30m --foreground 30m ./bin/wayang-submit -Xmx32g org.apache.wayang.ml.benchmarks.JOBenchmark java,spark,flink,postgres $data_path/ $timings_path/ $query
+        if [ $? -eq 124 ]; then
+            echo "Query ${query} timed out"
+        fi
     done
 
     #for query in "$test_path"/*.sql; do
