d274720ed6ac73cf62dba17b57eb3b5a63480bee
mspruc <madssejer@gmail.com>
2025-03-18 08:55:45 +0100

    clean up unchecked casts

--- wayang-platforms/wayang-postgres/src/main/java/org/apache/wayang/postgres/mapping/GlobalReduceMapping.java
+++ wayang-platforms/wayang-postgres/src/main/java/org/apache/wayang/postgres/mapping/GlobalReduceMapping.java
@@ -1,14 +1,13 @@
 package org.apache.wayang.postgres.mapping;
 
 import org.apache.wayang.basic.operators.GlobalReduceOperator;
+import org.apache.wayang.basic.data.Record;
 import org.apache.wayang.core.mapping.Mapping;
 import org.apache.wayang.core.mapping.OperatorPattern;
 import org.apache.wayang.core.mapping.PlanTransformation;
 import org.apache.wayang.core.mapping.ReplacementSubplanFactory;
 import org.apache.wayang.core.mapping.SubplanPattern;
 import org.apache.wayang.core.types.DataSetType;
-import org.apache.wayang.java.operators.JavaGlobalReduceOperator;
-import org.apache.wayang.java.platform.JavaPlatform;
 import org.apache.wayang.postgres.operators.PostgresGlobalReduceOperator;
 import org.apache.wayang.postgres.platform.PostgresPlatform;
 
@@ -16,9 +15,9 @@ import java.util.Collection;
 import java.util.Collections;
 
 /**
- * Mapping from {@link GlobalReduceOperator} to {@link PostgresGlobalReduceOperator}.
+ * Mapping from {@link GlobalReduceOperator} to
+ * {@link PostgresGlobalReduceOperator}.
  */
-@SuppressWarnings("unchecked")
 public class GlobalReduceMapping implements Mapping {
 
     @Override
@@ -26,19 +25,20 @@ public class GlobalReduceMapping implements Mapping {
         return Collections.singleton(new PlanTransformation(
                 this.createSubplanPattern(),
                 this.createReplacementSubplanFactory(),
-                PostgresPlatform.getInstance()
-        ));
+                PostgresPlatform.getInstance()));
     }
 
+
     private SubplanPattern createSubplanPattern() {
-        final OperatorPattern operatorPattern = new OperatorPattern(
-                "reduce", new GlobalReduceOperator<>(null, DataSetType.none()), false);
+        final OperatorPattern<GlobalReduceOperator<Record>> operatorPattern = new OperatorPattern<>(
+                "reduce",
+                new GlobalReduceOperator<>(null, DataSetType.createDefault(Record.class)),
+                false);
         return SubplanPattern.createSingleton(operatorPattern);
     }
 
     private ReplacementSubplanFactory createReplacementSubplanFactory() {
-        return new ReplacementSubplanFactory.OfSingleOperators<GlobalReduceOperator>(
-                (matchedOperator, epoch) -> new PostgresGlobalReduceOperator<>(matchedOperator).at(epoch)
-        );
+        return new ReplacementSubplanFactory.OfSingleOperators<GlobalReduceOperator<Record>>(
+                (matchedOperator, epoch) -> new PostgresGlobalReduceOperator<>(matchedOperator).at(epoch));
     }
 }
