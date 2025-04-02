a01613e0c8e1b671461f7f2f2c2ad33df0b41644
mspruc <madssejer@gmail.com>
2025-04-02 13:00:58 +0200

    fix missing interface

--- wayang-platforms/wayang-postgres/src/main/java/org/apache/wayang/postgres/operators/PostgresCartesianOperator.java
+++ wayang-platforms/wayang-postgres/src/main/java/org/apache/wayang/postgres/operators/PostgresCartesianOperator.java
@@ -4,7 +4,7 @@ import org.apache.wayang.basic.operators.CartesianOperator;
 import org.apache.wayang.jdbc.operators.JdbcCartesianOperator;
 import org.apache.wayang.basic.data.Record;
 
-public class PostgresCartesianOperator extends JdbcCartesianOperator {
+public class PostgresCartesianOperator extends JdbcCartesianOperator implements PostgresExecutionOperator {
 
     public PostgresCartesianOperator(CartesianOperator<Record, Record> that) {
         super(that);
