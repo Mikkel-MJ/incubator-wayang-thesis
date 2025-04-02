bbedc0123f0161ea1595d0faf6c8dfe203db63f0
mspruc <madssejer@gmail.com>
2025-04-02 12:20:34 +0200

    cleanup of sort visitor

--- wayang-api/wayang-api-sql/src/main/java/org/apache/wayang/api/sql/calcite/converter/WayangSortVisitor.java
+++ wayang-api/wayang-api-sql/src/main/java/org/apache/wayang/api/sql/calcite/converter/WayangSortVisitor.java
@@ -3,9 +3,15 @@ package org.apache.wayang.api.sql.calcite.converter;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.apache.calcite.rel.RelNode;
+import org.apache.calcite.rel.RelCollation;
+import org.apache.calcite.rel.RelFieldCollation.Direction;
+import org.apache.calcite.rex.RexInputRef;
+import org.apache.calcite.rex.RexLiteral;
 import org.apache.calcite.rex.RexNode;
+
 import org.apache.wayang.basic.data.Record;
+import org.apache.wayang.basic.operators.SortOperator;
+import org.apache.wayang.api.sql.calcite.converter.sorthelpers.KeyExtractor;
 import org.apache.wayang.api.sql.calcite.rel.WayangSort;
 import org.apache.wayang.api.sql.calcite.utils.AliasFinder;
 import org.apache.wayang.core.function.TransformationDescriptor;
@@ -13,27 +19,42 @@ import org.apache.wayang.core.plan.wayangplan.Operator;
 
 public class WayangSortVisitor extends WayangRelNodeVisitor<WayangSort> {
 
-    WayangSortVisitor(WayangRelConverter wayangRelConverter, AliasFinder aliasFinder) {
+    WayangSortVisitor(final WayangRelConverter wayangRelConverter, final AliasFinder aliasFinder) {
         super(wayangRelConverter, aliasFinder);
     }
 
     @Override
-    Operator visit(WayangSort wayangRelNode) {
-        assert(wayangRelNode.getInputs().size() == 1) : "Sorts must only have one input, but found: " + wayangRelNode.getInputs().size();
-        final Operator childOp = wayangRelConverter.convert(wayangRelNode.getInput(), aliasFinder);
-        
-        final List<RexNode> sorts = wayangRelNode.getSortExps();
-        final RexNode fetch = wayangRelNode.fetch;
-        final RexNode offset = wayangRelNode.offset;
-        
-        System.out.println("sorts: " + sorts);
-        System.out.println("fetch: " + fetch);
-        System.out.println("offset: " + offset);
+    Operator visit(final WayangSort wayangRelNode) {
+        assert (wayangRelNode.getInputs().size() == 1)
+                : "Sorts must only have one input, but found: " + wayangRelNode.getInputs().size();
 
-        final TransformationDescriptor<?,?> td = new TransformationDescriptor<>(null, Record.class, Object.class);
-        
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        final Operator childOp = wayangRelConverter.convert(wayangRelNode.getInput(), aliasFinder);
+
+        final RexNode fetch = wayangRelNode.fetch;
+        final RexLiteral offset = (RexLiteral) wayangRelNode.offset;
+        final RelCollation collation = wayangRelNode.getCollation();
+
+        final List<Direction> collationDirections = collation.getFieldCollations().stream()
+                .map(fieldCol -> fieldCol.getDirection())
+                .collect(Collectors.toList());
+
+        final List<Integer> collationIndexes = collation.getFieldCollations().stream()
+                .map(fieldCol -> fieldCol.getFieldIndex())
+                .collect(Collectors.toList());
+
+        final TransformationDescriptor<Record, Record> td = new TransformationDescriptor<Record, Record>(
+                new KeyExtractor(
+                        collationDirections,
+                        collationIndexes,
+                        fetch,
+                        RexLiteral.intValue(offset)),
+                Record.class, Record.class);
+
+        final SortOperator<Record, Record> sort = new SortOperator<Record, Record>(td);
+
+        childOp.connectTo(0, sort, 0);
+
+        return sort;
     }
-    
+
 }
