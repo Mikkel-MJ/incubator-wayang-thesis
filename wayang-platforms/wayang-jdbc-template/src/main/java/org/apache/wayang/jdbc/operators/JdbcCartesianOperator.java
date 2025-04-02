621b45321d9c1cd07796e2928a11f5c1e17dc696
mspruc <madssejer@gmail.com>
2025-04-02 12:19:31 +0200

    exception for creating sql clauses directly on jdbc cartesians

--- wayang-platforms/wayang-jdbc-template/src/main/java/org/apache/wayang/jdbc/operators/JdbcCartesianOperator.java
+++ wayang-platforms/wayang-jdbc-template/src/main/java/org/apache/wayang/jdbc/operators/JdbcCartesianOperator.java
@@ -1,13 +1,11 @@
 package org.apache.wayang.jdbc.operators;
 
 import java.sql.Connection;
-
 import org.apache.wayang.basic.operators.CartesianOperator;
 import org.apache.wayang.jdbc.compiler.FunctionCompiler;
-import org.apache.wayang.jdbc.platform.JdbcPlatformTemplate;
 import org.apache.wayang.basic.data.Record;
 
-public class JdbcCartesianOperator extends CartesianOperator<Record, Record> implements JdbcExecutionOperator {
+public abstract class JdbcCartesianOperator extends CartesianOperator<Record, Record> implements JdbcExecutionOperator {
 
     public JdbcCartesianOperator(final CartesianOperator<Record, Record> that) {
         super(that);
@@ -15,11 +13,7 @@ public class JdbcCartesianOperator extends CartesianOperator<Record, Record> imp
 
     @Override
     public String createSqlClause(final Connection connection, final FunctionCompiler compiler) {
-        return "CROSS JOIN";
-    }
-
-    @Override
-    public JdbcPlatformTemplate getPlatform() {
-        throw new UnsupportedOperationException("Unimplemented method 'getPlatform'");
+        throw new UnsupportedOperationException(
+                "create sql clause not directly supported since operators may not contain information about children, you will have to implement your own recursion to find tables relevant to sql lingo: CROSS JOIN on \'table\'");
     }
 }
