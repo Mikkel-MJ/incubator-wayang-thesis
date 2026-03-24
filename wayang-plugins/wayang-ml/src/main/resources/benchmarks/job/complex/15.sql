555807ef37cad49116fbff014641a1c6d05336f4
Juri Petersen <juri@hey.com>
2026-03-24 09:53:13 +0100

    Remove duplicate benchmarks

--- wayang-plugins/wayang-ml/src/main/resources/benchmarks/job/complex/15.sql
+++ wayang-plugins/wayang-ml/src/main/resources/benchmarks/job/complex/15.sql
@@ -1,8 +1,8 @@
 SELECT
-    MIN(at.title) AS aka_title,
+    MIN(at_alias.title) AS aka_title,
     MIN(t.title) AS internet_movie_title
 FROM
-    postgres.aka_title AS at,
+    postgres.aka_title AS at_alias,
     postgres.company_name AS cn,
     postgres.company_type AS ct,
     postgres.info_type AS it1,
@@ -16,16 +16,16 @@ WHERE
     it1.info = 'release dates' AND
     mi.note LIKE '%internet%' AND
     t.production_year > 1990 AND
-    t.id = at.movie_id AND
+    t.id = at_alias.movie_id AND
     t.id = mi.movie_id AND
     t.id = mk.movie_id AND
     t.id = mc.movie_id AND
     mk.movie_id = mi.movie_id AND
     mk.movie_id = mc.movie_id AND
-    mk.movie_id = at.movie_id AND
+    mk.movie_id = at_alias.movie_id AND
     mi.movie_id = mc.movie_id AND
-    mi.movie_id = at.movie_id AND
-    mc.movie_id = at.movie_id AND
+    mi.movie_id = at_alias.movie_id AND
+    mc.movie_id = at_alias.movie_id AND
     k.id = mk.keyword_id AND
     it1.id = mi.info_type_id AND
     cn.id = mc.company_id AND
--- wayang-plugins/wayang-ml/src/main/resources/benchmarks/job/complex/18.sql
+++ wayang-plugins/wayang-ml/src/main/resources/benchmarks/job/complex/18.sql
@@ -2,7 +2,7 @@ SELECT
     MIN(mi.info) AS release_date,
     MIN(t.title) AS modern_american_internet_movie
 FROM
-    postgres.aka_title AS at,
+    postgres.aka_title AS at_alias,
     postgres.company_name AS cn,
     postgres.company_type AS ct,
     postgres.info_type AS it1,
@@ -18,16 +18,16 @@ WHERE
     mi.info IS NOT NULL AND
     (mi.info LIKE 'USA:% 199%' OR mi.info LIKE 'USA:% 200%') AND
     t.production_year > 1990 AND
-    t.id = at.movie_id AND
+    t.id = at_alias.movie_id AND
     t.id = mi.movie_id AND
     t.id = mk.movie_id AND
     t.id = mc.movie_id AND
     mk.movie_id = mi.movie_id AND
     mk.movie_id = mc.movie_id AND
-    mk.movie_id = at.movie_id AND
+    mk.movie_id = at_alias.movie_id AND
     mi.movie_id = mc.movie_id AND
-    mi.movie_id = at.movie_id AND
-    mc.movie_id = at.movie_id AND
+    mi.movie_id = at_alias.movie_id AND
+    mc.movie_id = at_alias.movie_id AND
     k.id = mk.keyword_id AND
     it1.id = mi.info_type_id AND
     cn.id = mc.company_id AND
